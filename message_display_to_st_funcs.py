3e4f0596fcfcb385dd947962b8472407b4d70eec
leahphw <leah.phw@gmail.com>
2025-03-02 11:50:38 -0500

    merge single display functions, include chat element wrapper

--- message_display_to_st_funcs.py
+++ message_display_to_st_funcs.py
@@ -25,91 +25,46 @@ def write_display_info(st, display_info, include_datetime: bool = True):
     else:
         st.write(f"{author} ({author_type})")
 
-def write_text_to_st(message: SinglePartMessage, st, include_datetime: bool = True):
+def write_single_chat_to_st(message: SinglePartMessage, st, include_datetime: bool = True):
+    """Write a single part message to streamlit
+
+    Args:
+        message (SinglePartMessage): Message object, Single
+        st (_type_): Streamlit
+        include_datetime (bool, optional): _description_. Defaults to True.
+
+    Raises:
+        ValueError: If message type is invalid
     """
-    Print a text message on the screen with Streamlit
-    
-    Returns:
-        None
-    """     
-    # Get the information
     display_info = get_display_info(message)
-
-    # Display the content
-    display_value = message.get_message_value_by_attribute("text")
+    author, author_type, display_datetime = display_info
     write_display_info(st, display_info, include_datetime)
-    st.write(display_value)
-
-def write_image_to_st(message: SinglePartMessage, st, include_datetime: bool = True):
-    """
-    Display an image message on Streamlit
-
-    Returns:
-        None
-    """
-    # Get the information
-    display_info = get_display_info(message)
     
-    # Get the image data
-    if message.get_message_type() == "image_base64":
-        image_data = message.get_message_value_by_attribute("image_base64")
-    elif message.get_message_type() == "image_url":
-        image_data = message.get_message_value_by_attribute("url")
-    else:
-        raise ValueError("Error parsing message type: image_base64 or image_url only")
-    filename = message.get_message_value_by_attribute("filename")
-
-    # Display the content
-    write_display_info(st, display_info, include_datetime)
-    st.image(image_data, caption=filename, use_container_width=True)
-
-
-def write_file_to_st(message: SinglePartMessage, st, include_datetime: bool = True):
-    """
-    Display a file message on Streamlit
-    
-    Returns:
-        None
-    """
-    # Get the information
-    display_info = get_display_info(message)
-
-    # Get the file data
-    if message.get_message_type() == "file_base64":
-        file_data = message.get_message_value_by_attribute("file_base64")
-    elif message.get_message_type() == "file_url":
-        file_data = message.get_message_value_by_attribute("url")
-    else:
-        raise ValueError("Error parsing message type: file_base64 or file_url only")
-    mime_type = message.get_message_value_by_attribute("mime_type")
-    filename = message.get_message_value_by_attribute("filename")
-
-    # Display the content
-    write_display_info(st, display_info, include_datetime)
-
-    # Save file and provide download link
-    st.download_button(label=f"Download {filename}", data=file_data, file_name=filename, mime=mime_type)
-
-def write_audio_to_st(message: SinglePartMessage, st, include_datetime: bool = True):
-    """
-    Display an audio message on Streamlit
-    
-    Returns:
-        None
-    """
-    # Get the information
-    display_info = get_display_info(message)
-
-    # Get the file data
-    if message.get_message_type() == "audio_base64":
-        audio_data = message.get_message_value_by_attribute("audio_base64")
-    elif message.get_message_type() == "audio_url":
-        audio_data = message.get_message_value_by_attribute("url")
-    else:
-        raise ValueError("Error parsing message type: audio_base64 or audio_url only")
-
-    # Display the content
-    write_display_info(st, display_info, include_datetime)
-
-    # Audio widget
-    st.audio(data=audio_data)
+    with st.chat_message(author_type):
+        message_type = message.get_message_type()
+        if message_type == "text":
+            text = message.get_message_value_by_attribute("text")
+            st.write(text)
+        elif message_type == "image_base64" or message_type == "image_url":
+            filename = message.get_message_value_by_attribute("filename")
+            if message_type == "image_base64":
+                image_data = message.get_message_value_by_attribute("image_base64")
+            else:
+                image_data = message.get_message_value_by_attribute("url")
+            st.image(image_data, caption=filename, use_container_width=True)
+        elif message_type == "file_base64" or message_type == "file_url":
+            filename = message.get_message_value_by_attribute("filename")
+            mime_type = message.get_message_value_by_attribute("mime_type")
+            if message_type == "file_base64":
+                file_data = message.get_message_value_by_attribute("file_base64")
+            else:
+                file_data = message.get_message_value_by_attribute("url")
+            st.download_button(label=f"Download {filename}", data=file_data, file_name=filename, mime=mime_type)
+        elif message_type == "audio_base64" or message_type == "audio_url":
+            if message_type == "audio_base64":
+                audio_data = message.get_message_value_by_attribute("audio_base64")
+            else:
+                audio_data = message.get_message_value_by_attribute("url")
+            st.audio(data=audio_data)
+        else:
+            raise ValueError("Invalid Message Type")
